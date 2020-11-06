fdd818364015648140e592d0ec0a6f1771e4e56b
alexkrushkova <alexkrushkova@gmail.com>
2020-11-06 10:08:33 +0100

    TypeScript Demo

---
+++ 01. Intro to Angular and Typescript/TypeScript Demo/dist/index.js
@@ -0,0 +1,10 @@
+var User = (function () {
+    function User(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return User;
+}());
+var ivan = new User('Ivan', 20);
+console.log(ivan.name);
+//# sourceMappingURL=index.js.map
---
+++ 01. Intro to Angular and Typescript/TypeScript Demo/src/index.ts
@@ -0,0 +1,6 @@
+class User {
+    constructor(public name: string, public age: number){}
+}
+
+const ivan = new User('Ivan', 20);
+console.log(ivan.name);
