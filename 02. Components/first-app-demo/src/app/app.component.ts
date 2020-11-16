83a55f9121d24fc7ae78dd84285e3d383c7552c0
alexkrushkova <alexkrushkova@gmail.com>
2020-11-16 08:43:14 +0100

    app updated

--- 02. Components/first-app-demo/src/app/app.component.ts
+++ 02. Components/first-app-demo/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { API_URL_TOKEN, MyService } from './providers';
+import { environment } from '../environments/environment';
+import { UserService } from './user.service';
+import { IUser } from './interfaces/user';
+const API_URL = environment.apiURL;
 
 const users = [
   {
@@ -15,33 +20,45 @@ const users = [
     firstName: 'First Name 3',
     lastName: 'Last Name 3',
     age: 33
-  },
+  }
 ];
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
+  users: IUser[] | null = null;
+
   welcomeMessage = 'Hello!';
 
-  nameInputValue = 'Test Test';
+  nameInputValue = 'TEST TEST';
 
   isVisible = false;
-  users = users;
 
-  nameInputBtnClickHandler(data: { inputEl: HTMLInputElement}):void {
+  data = {
+    name: 'TEST'
+  };
+
+  nameInputBtnClickHandler(data: { inputEl: HTMLInputElement }): void {
     console.log(data.inputEl.value);
   }
 
-  constructor(){
 
+  constructor(public userService: UserService) {
+    userService.loadUsers().subscribe(u => { this.users = u; });
+    // console.log(userService);
+    // setTimeout(() => {
+    //   // this.data.name = 'HELLO!';
+    //   this.data = { name: 'HELLO!' };
+    //   console.log('change!');
+    //   // this.nameInputValue = 'BEST!';
+    // }, 4000);
   }
 
-  toggleHandler():void {
+  toggleHandler(): void {
     this.isVisible = !this.isVisible;
   }
-
 }
--- 02. Components/first-app-demo/src/app/name-input/name-input.component.ts
+++ 02. Components/first-app-demo/src/app/name-input/name-input.component.ts
@@ -1,40 +1,57 @@
-import { Component, OnDestroy, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
+
+
+
+
+
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy
+} from '@angular/core';
 
 @Component({
   selector: 'app-name-input',
   templateUrl: './name-input.component.html',
-  styleUrls: ['./name-input.component.css']
+  styleUrls: ['./name-input.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NameInputComponent implements OnDestroy, OnInit, OnChanges {
 
- @Input() inputValue = 'Default Value';
- @Output() btnClick = new EventEmitter();
+  @Input() inputValue = 'Default Value';
+  // @Input() article: IArticle;
+  @Output() btnClick = new EventEmitter();
 
- constructor() { }
+  constructor() {
+    console.log('ctor');
+  }
 
   ngOnInit(): void {
 
   }
 
-  ngOnChanges(simpleChanges: SimpleChanges):void{
+  ngOnChanges(simpleChanges: SimpleChanges): void {
     console.log(simpleChanges);
-    
   }
 
-  // inputKeyupHandler(target: HTMLInputElement): void{
+  // inputKeyupHandler(target: HTMLInputElement): void {
   //   this.inputValue = target.value;
   // }
 
-  btnClickHandler(value: KeyboardEvent, inputEl: HTMLInputElement):void {
+  btnClickHandler(value: KeyboardEvent, inputEl: HTMLInputElement): void {
     console.log('btn was clicked!', value);
     console.log('input value is', inputEl.value);
 
     this.btnClick.emit({ inputEl });
   }
 
-  ngOnDestroy():void{
-    console.log('name input component was destroyed!');
-    
+  ngOnDestroy(): void {
+    console.log('name input component was destroyed');
   }
-}
 
+}
